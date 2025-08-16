8078f469ab0e62fd42f159e73bbe5bd3223601ba
Nenia Hidalgo <neniahidalgo@gmail.com>
2025-08-16 10:36:45 -0300

    primer commit

---
+++ clase 11 files/party.py
@@ -0,0 +1,16 @@
+class PartyAnimal:
+    x = 0 # atributo de clase
+
+    def party(self) : # método de instancia
+        self.x = self.x + 1
+        print("Hasta ahora", self.x)
+
+# instancia de la clase PartyAnimal
+an = PartyAnimal()
+
+# Llamadas al método party
+an.party()
+an.party()
+an.party()
+print (type(an))
+print (dir(an))
